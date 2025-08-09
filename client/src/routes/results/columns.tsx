0c029a29e3d56bb2219340c69d4facabefa23777
JamieBliss <zfac223@live.rhul.ac.uk>
2025-08-09 20:41:27 +0100

    Implemented basic datatable to get files

---
+++ client/src/routes/results/columns.tsx
@@ -0,0 +1,54 @@
+import {
+  type ColumnDef,
+} from "@tanstack/react-table"
+import { ArrowUpDown} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import type { SchemaFileRecordWithClassifications, SchemaFileClassificationRead } from "@/types/types"
+import { StatusDot } from "./status-dot"
+
+
+export const columns: ColumnDef<SchemaFileRecordWithClassifications>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <StatusDot fileStatus={row.getValue("status")} />
+    ),
+  },
+  {
+    accessorKey: "filename",
+    enableSorting: true,
+    enableColumnFilter: true,
+    cell: ({ row }) => <div>{row.getValue("filename")}</div>,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Filename
+          <ArrowUpDown />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: "classifications",
+    id:"classification_name",
+    header: "Classification",
+    cell: ({ row }) => {
+      const classifications = row.getValue("classifications") as SchemaFileClassificationRead[];
+      return (
+        <div>{classifications.length > 0 ? classifications[0].classification : ''}</div>) }
+  },
+  {
+    accessorKey: "classifications",
+    header: "Classification Score",
+    enableSorting: true,
+    cell: ({ row }) => {
+      const classifications = row.getValue("classifications") as SchemaFileClassificationRead[];
+      return (
+        <div>{classifications.length > 0 ? classifications[0].classification_score : ''}</div>) }
+  },
+]
---
+++ client/src/routes/results/status-dot.tsx
@@ -0,0 +1,27 @@
+import { FileStatus } from "@/types/types";
+
+export const StatusDot = ({fileStatus} : {fileStatus: FileStatus}) => {
+  let colorClass;
+
+  switch (fileStatus) {
+    case FileStatus.Failed:
+      colorClass = 'bg-red-500';
+      break;
+    case FileStatus.Processing:
+      colorClass = 'bg-yellow-500';
+      break;
+    case FileStatus.Completed:
+      colorClass = 'bg-green-500';
+      break;
+    default:
+      colorClass = 'bg-gray-300'; // fallback color
+  }
+
+  return (
+    <span
+      className={`capitalize flex w-2 h-2 rounded-full ${colorClass} items-center`}
+      aria-label={`Status: ${fileStatus}`}
+      title={fileStatus}
+    />
+  );
+};
