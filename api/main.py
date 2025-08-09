f51ab68525319bcd6fea3604a5d4f4651ed75155
JamieBliss <zfac223@live.rhul.ac.uk>
2025-08-09 14:07:01 +0100

    Add CORS

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session
 
 from .database import create_db_and_tables, get_session
@@ -7,7 +8,20 @@ from .routers import files as file_routes
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(file_routes.router)
 
 
